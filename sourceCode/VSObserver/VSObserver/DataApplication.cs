78421f5b271c2192ad8f1bf18a015d97fa262b3f
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-02-16 16:45:03 +0100

    Add DataApplication class

---
+++ sourceCode/VSObserver/VSObserver/DataApplication.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSObserver
+{
+    class DataApplication : ViewModelBase
+    {
+        private const string LOAD_DONE = "LoadDone";
+
+        private bool _ldone;
+
+        public bool LoadDone
+        {
+            get { return _ldone; }
+            set { _ldone = value; OnPropertyChanged(LOAD_DONE); }
+        }
+    }
+}
