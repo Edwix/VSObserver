cfd1a4c3056d46d915505399594dbbfe72cce484
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-02-16 09:38:57 +0100

    The value are center. The color of value cell changes.

---
+++ sourceCode/VSObserver/VSObserver/DataObserver.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace VSObserver
+{
+    /// <summary>
+    /// Classe qui permet de récupérer les données brutes
+    /// </summary>
+    class DataObserver : ViewModelBase
+    {
+        private const string PATH = "Path";
+        private const string VARIABLE = "Variable";
+        private const string TYPE = "Type";
+        private const string VALUE = "Value";
+        private const string TIMESTAMP = "Timestamp";
+        private const string HAS_CHANGED = "ValueHasChanged";
+
+        private string _path;
+        private string _var;
+        private string _ts;
+        private string _val;
+        private bool _hasChanged;
+
+        public string Path
+        { 
+            get { return _path; } 
+            set { _path = value; OnPropertyChanged(PATH); } 
+        }
+
+        public string Variable
+        {
+            get { return _var; }
+            set { _var = value; OnPropertyChanged(VARIABLE); }
+        }
+
+        public string Value
+        {
+            get { return _val; }
+            set { _val = value; OnPropertyChanged(VALUE); }
+        }
+
+        public string Timestamp
+        {
+            get { return _ts; }
+            set { _ts = value; OnPropertyChanged(Timestamp); }
+        }
+
+        public bool ValueHasChanged
+        {
+            get { return _hasChanged; }
+            set { _hasChanged = value; OnPropertyChanged(HAS_CHANGED); }
+        }
+    }
+}
---
+++ sourceCode/VSObserver/VSObserver/ValueToBackgroundConverter.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Globalization;
+using System.Windows.Data;
+using System.Windows.Media;
+
+namespace VSObserver
+{
+    class ValueToBackgroundConverter : IValueConverter
+    {
+        public object Convert(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            if (value is bool)
+            {
+                bool hasChanged = (bool)value;
+
+                if (hasChanged)
+                {
+                    return Brushes.LightBlue;
+                }
+                else
+                {
+                    return Brushes.Transparent;
+                }
+            }
+            //value is not an integer. Do not throw an exception in the converter, but return something that is obviousl ywrong
+            return Brushes.Transparent;
+        }
+
+        public object ConvertBack(object value, Type targetType, object parameter, CultureInfo culture)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
