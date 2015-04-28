058a4f020d3b2303e736a7aee2a0ab097c7fe6fb
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-04-28 08:51:52 +0200

    Timestamp modification

--- sourceCode/VSObserver/VSObserver/VariableObserver.cs
+++ sourceCode/VSObserver/VSObserver/VariableObserver.cs
@@ -533,9 +533,9 @@ namespace VSObserver
         /// <returns></returns>
         private DataObserver createDataObserver(string path, string value, long timeStamp, string mapping, bool forced)
         {
-            System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
-            dtDateTime = dtDateTime.AddSeconds(timeStamp);
-            double epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
+            //System.DateTime dtDateTime = new DateTime(1970, 1, 1, 0, 0, 0, System.DateTimeKind.Utc);
+            //dtDateTime = dtDateTime.AddSeconds(timeStamp);
+            //double epoch = (DateTime.UtcNow - new DateTime(1970, 1, 1)).TotalSeconds;
 
             DataObserver dObs = new DataObserver {
                 PathName = path,
