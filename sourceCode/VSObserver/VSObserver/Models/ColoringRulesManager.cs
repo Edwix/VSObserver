804618367e6e75f5b026d18b757e394ebced3290
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-07-03 08:25:39 +0200

    Ticket #12 : Editing color rule from VSObserver Add of binding regex, comment and the error of regex.

--- sourceCode/VSObserver/VSObserver/Models/ColoringRulesManager.cs
+++ sourceCode/VSObserver/VSObserver/Models/ColoringRulesManager.cs
@@ -4,14 +4,23 @@ using System.Linq;
 using System.Text;
 using System.Collections.ObjectModel;
 using System.Windows.Input;
+using System.Text.RegularExpressions;
 
 namespace VSObserver.Models
 {
     class ColoringRulesManager : ViewModelBase
     {
         private const string LIST_COLORINGRULES = "ListOfColoringRules";
+        private const string RULE_REGEX = "RuleRegex";
+        private const string REGEX_ERROR = "RegexError";
+        private const string RULE_COMMENT = "RuleComment";
 
         private ObservableCollection<ColoringRules> _listOfColoringRules;
+
+        private string _regex;
+        private string _comment;
+        private string _regexErr;
+
         private ICommand cmdAddColRul;
 
         public ColoringRulesManager ()
@@ -23,6 +32,33 @@ namespace VSObserver.Models
         {
             get { return _listOfColoringRules; }
             set { _listOfColoringRules = value; OnPropertyChanged(LIST_COLORINGRULES); }
+        }
+
+        public string RuleRegex
+        {
+            get { return _regex; }
+            set { _regex = value; OnPropertyChanged(REGEX_ERROR); OnPropertyChanged(RULE_REGEX); }
+        }
+
+        public string RuleComment
+        {
+            get { return _comment; }
+            set { _comment = value; OnPropertyChanged(RULE_COMMENT); }
+        }
+
+        public string RegexError
+        {
+            get 
+            {
+                bool isValid = IsValidRegex(_regex);
+
+                if (!isValid)
+                    _regexErr = "The regular expression isn't correct !";
+                else
+                    _regexErr = null;
+
+                return _regexErr; 
+            }
         }
 
         public ICommand AddColoringRule
@@ -42,6 +78,20 @@ namespace VSObserver.Models
             {
                 _listOfColoringRules.Add(new ColoringRules());
             }
+        }
+
+        private static bool IsValidRegex(string pattern)
+        {
+            try
+            {
+                Regex.Match("", pattern);
+            }
+            catch (ArgumentException)
+            {
+                return false;
+            }
+
+            return true;
         }
     }
 }
