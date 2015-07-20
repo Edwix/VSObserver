703f42f44f8f6345b12b0684ce51a8e58cdd79bb
Edwin MOULIN <edwin.moulin@transport.alstom.com>
2015-07-20 11:23:19 +0200

    Ticket #30: Multiple forcing / writing Creation of dialog which able to enter a value for selected variables

---
+++ sourceCode/VSObserver/VSObserver/Dialog/PutValueDialog.xaml.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace VSObserver.Dialog
+{
+    /// <summary>
+    /// Logique d'interaction pour PutValueDialog.xaml
+    /// </summary>
+    public partial class PutValueDialog : Window
+    {
+        public PutValueDialog()
+        {
+            InitializeComponent();
+
+            //Affichage en premier plan
+            this.Topmost = true;
+        }
+    }
+}
